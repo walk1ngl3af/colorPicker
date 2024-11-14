f688f5e084b2e00883aa77144a63d1d38e01564e
s3hw <s3hw009@gmail.com>
2024-11-14 08:43:15 -0800

    Changed the original text color

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', {
     color: "#FFFFFF",
-    textColor: "#FFFFFF"
+    textColor: "#000000"
   });
 });
 
